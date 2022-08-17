bdb7642e81e1ce448d111e5d9209ad4187a31851
iscyy <wenvoi@163.com>
2022-08-17 19:02:46 +0800

    update

--- docs/.vuepress/sidebar.js
+++
@@ -1,6 +0,0 @@
-module.exports = {//这是主配置文件
-	'/guide/': require('../guide/sidebar'),
-
-	'/baodian/zero': require('../baodian/zero/sidebar'),
-	'/baodian/high': require('../baodian/high/sidebar'),
-}	
